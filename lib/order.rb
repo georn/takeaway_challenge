337d0c70d90511d68e609361d09ecb976e716961
Guillermo <mrog0@outlook.com>
2017-05-21 15:28:42 +0100

    Add #add to Order class along with its Unit tests

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,15 @@
 class Order
   attr_reader :bill
 
+  # REMEMBER TO TAKE OUT
+  PRICE = 1
+
   def initialize
     @bill = []
   end
 
-  def show
-    bill
+  def add(dish_name, amount)
+    bill.push(dish: dish_name, price: amount * PRICE )
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,9 +4,30 @@ describe Order do
 
   let(:order) { subject }
 
-  describe "#show" do
+  describe "#bill" do
     it "shows an empty bill" do
-      expect(order.show).to eq []
+      expect(order.bill).to eq []
+    end
+  end
+
+  describe "#add_order" do
+    it "adds one request to the bill" do
+      order.add("arepa", 1)
+      expect(order.bill.count).to eq 1
+    end
+
+    it "adds two request to the bill" do
+      order.add("arepa", 2)
+      order.add("cachapa", 1)
+      expect(order.bill.count).to eq 2
+    end
+  end
+
+  describe "#show" do
+    xit "outputs in the console the bill with the dish name and the price" do
+      order.add("arepa", 2)
+      order.add("cachapa", 3)
+      order.show
     end
   end
 end
